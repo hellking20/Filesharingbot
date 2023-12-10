4434dfe18c2473bab93d42a1851191c4a3eec86c
hellking20 <123181331+hellking20@users.noreply.github.com>
2023-12-10 07:20:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,34 @@
-rm -rf boted;
-git clone https://github.com/hellking20/Filesharingbot;
-cd boted;
-pip3 install -U -r requirements.txt;
-echo "Bot Started....";
-python3 bot.py;
+if [ -z $UPSTREAM_REPO ]
+
+
+then
+
+
+echo "Cloning main Repository"
+
+git clone https://github.com/hellking20/Filesharingbot/Filesharingbot
+
+
+
+else
+
+
+
+echo "Cloning Custom Repo from $UPSTREAM_REPO "
+
+git clone SUPSTREAM REPO /Filesharingbot
+
+fi
+
+
+cd /Filesharingbot
+
+
+
+pip3 install -U -r requirements.txt
+
+echo "Bot Started...."
+
+
+
+python3 bot.py
